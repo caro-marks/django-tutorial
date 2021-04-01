5526331fb1b1ce7ed59442e8aa5a0c7e2b36c8e0
Marcos Nolasco <mksalex08@gmail.com>
2021-03-31 22:26:15 -0300

    ok

--- retratos/urls.py
+++ retratos/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = 'retratos'
 urlpatterns = [
-    path('', views.index, name='index'),  # ex: /retratos/
-    path('<int:question_id>', views.detail, name='detail'),  # ex: /retratos/5/
-    path('<int:question_id>/results/', views.results,
+    path('', views.IndexView.as_view(), name='index'),  # ex: /retratos/
+    path('<int:pk>', views.DetailView.as_view(),
+         name='detail'),  # ex: /retratos/5/
+    path('<int:pk>/results/', views.ResultsView.as_view(),
          name='results'),  # ex: /retratos/5/results
     path('<int:question_id>/vote/', views.vote,
          name='vote'),  # ex: /retratos/5/vote
